74c751d979381ab6b9f38291e1dd6e0976860994
hitsan <soledewa2828@gmail.com>
2021-12-08 14:23:07 +0900

    fix key

--- work/mysite/mysite/settings.py
+++ work/mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a=8%ax+#bx_^exhq*y9&esblhhpktk0slys*wj++qje52ld3s&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
